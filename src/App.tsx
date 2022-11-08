55fb60fffd314d76e564752afad307a447f522e0
lasthope1 <divinity3191@gmail.com>
2022-11-08 20:56:41 +0700

    deploy2

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import { BrowserRouter as Router, Route, Routes,} from 'react-router-dom';
 const link = createHttpLink({
   // uri: "http://localhost:3000/graphql",
   uri: "https://curriculum-backend.onrender.com/graphql/",
-  credentials: 'omit'
+  credentials: 'include'
 });
 
 const client = new ApolloClient({
